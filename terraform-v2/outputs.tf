aaf23200388eb18adba5aa855decf7082f97775c
wishmstr <ryuks.apples@gmail.com>
2025-02-27 02:38:23 +0000

    Fixed error

--- terraform-v2/outputs.tf
+++ terraform-v2/outputs.tf
@@ -33,11 +33,11 @@ output "database_port" {
 }
 
 output "s3_bucket_arn" {
-value = aws_s3_bucket.terraform_state.arn
-description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
 }
 
 output "dynamodb_table_name" {
-value = aws_dynamodb_table.terraform_lock.name
-description = "The name of the DynamoDB table"
+  value       = aws_dynamodb_table.terraform_lock.name
+  description = "The name of the DynamoDB table"
 }
