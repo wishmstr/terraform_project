4867767e6440759b96e681a5b3f9e79bcc07f56a
wishmstr <ryuks.apples@gmail.com>
2025-02-28 04:31:17 +0000

    Workflow tweaks

--- terraform-v2/01_provider.tf
+++ terraform-v2/01_provider.tf
@@ -11,21 +11,24 @@ terraform {
   backend "s3" {
     bucket         = "wishmstr-terraform-state-bucket" # The name of the S3 bucket
     key            = "global/s3/terraform.tfstate"     # The path to your state file in the bucket
-    region         = "us-east-2"                       # The region where the S3 bucket is located
+    region         = var.aws_region                    # The region where the S3 bucket is located
     encrypt        = true                              # Enable server-side encryption
     dynamodb_table = "terraform-lock"                  # Optional: DynamoDB table for state locking
     acl            = "private"                         # S3 bucket ACL, default is "private"
+    assume_role {
+      role_arn = var.aws_terraform_arn
+    }
   }
 }
 
 provider "aws" {
-  region = var.aws_region
+  # region = var.aws_region
 
-  access_key = var.aws_secret_access_key
-  secret_key = var.aws_secret_key_id
-  assume_role {
-    role_arn = var.aws_terraform_arn
-  }
+  # access_key = var.aws_secret_access_key
+  # secret_key = var.aws_secret_key_id
+  # assume_role {
+  #   role_arn = var.aws_terraform_arn
+  # }
 }
 
 data "aws_availability_zones" "available" {
